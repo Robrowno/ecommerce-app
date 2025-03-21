9d83ab7ef3f22efdc90171f0a05a6a949145962f
Samuel Javorka <samkoj@hotmail.co.uk>
2025-03-21 23:22:36 +0000

    bugfix serve static files for /ecomm-images

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ dotenv.config();
 
 const app = express();
 
+app.use("/ecomm-images", express.static(path.join(__dirname, "ecomm-images")));
+
 const productRoutes = require("./routes/productRoutes");
 app.use("/api/products", productRoutes);
-
 
 // Middleware
 app.use(express.json());
@@ -20,20 +21,20 @@ app.use(morgan("dev"));
 
 // Connect to MongoDB
 if (process.env.NODE_ENV !== "test") {
-  mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("✅ MongoDB connected"))
-    .catch((err) => console.error("❌ MongoDB connection error:", err));
+	mongoose
+		.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(() => console.log("✅ MongoDB connected"))
+		.catch((err) => console.error("❌ MongoDB connection error:", err));
 }
 
 // Sample Route
 app.get("/", (req, res) => {
-  res.send("API is running...");
+	res.send("API is running...");
 });
 
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 5001;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+	const PORT = process.env.PORT || 5001;
+	app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
 
 module.exports = app;
