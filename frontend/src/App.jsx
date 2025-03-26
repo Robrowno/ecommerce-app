8b17613b622d7ed03a5c71141b03d11b29a71dd2
konstantinos koliolios <kolioliosk@gmail.com>
2025-03-26 12:22:57 +0000

    Update App.jsx with auth routes and protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,40 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./screens/Home.jsx";
 import Products from "./screens/Products.jsx";
 import NavBar from "./components/NavBar.jsx";
 import About from "./screens/About.jsx";
 import { CartProvider } from "./context/CartContext";
+import Register from "./screens/Register.jsx";
+import Login from "./screens/Login.jsx";
+import { useAuth } from "./context/AuthContext";
 import "./index.css";
 
 const App = () => {
-	return (
-		<CartProvider>
-			<NavBar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/products" element={<Products />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</CartProvider>
-	);
+  const { user } = useAuth();
+
+  return (
+    <CartProvider>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? <Home /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            user ? <Products /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </CartProvider>
+  );
 };
 
 export default App;
